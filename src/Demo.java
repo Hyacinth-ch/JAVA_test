1223d1b83731ac0f44ed9a8d7ffce2dcdb76a7c1
sunrisesunout <1070649773@qq.com>
2018-09-27 20:50:49 +0800

    Demo_List

---
+++ src/Demo.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class Demo {
+    public static void main(String[] args) {
+	
+    	List<Integer> list=new ArrayList<>(1);
+    	Integer integer=new Integer(10);
+    	list.add(integer);
+    	Integer integer1=list.get(0);
+    	System.out.println(integer1); //10
+    	integer1=3;
+    	System.out.println(list.get(0));  //10
+    	
+   	  
+    }
+}
